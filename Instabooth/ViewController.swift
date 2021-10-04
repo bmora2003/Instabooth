aec753f3756c1aeb00be36e74fe626ebf21a9bc4
bmora2003 <bryanldindon@gmail.com>
2021-10-03 21:07:35 -0400

    Initial Commit

---
+++ Instabooth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instabooth
+//
+//  Created by Bryan Mora  on 10/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
